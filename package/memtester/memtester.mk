a256e71f2d18ab48fa4a136535e43410f3900f50
Tomasz Gorochowik <tgorochowik@antmicro.com>
2017-02-24 13:33:15 +0100

    Revert "memtester: change download site to enclustra"
    This reverts commit 48a81167b11401288613279f5b8603958643d97c.

--- package/memtester/memtester.mk
+++ package/memtester/memtester.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MEMTESTER_VERSION = 4.3.0
-MEMTESTER_SITE = ${BR2_ENCLUSTRA_SITE}
+MEMTESTER_SITE = http://pyropus.ca/software/memtester/old-versions
 MEMTESTER_LICENSE = GPLv2
 MEMTESTER_LICENSE_FILES = COPYING
 
