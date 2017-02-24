f147c70ff63497b235b29c7241f3eb590526c737
Tomasz Gorochowik <tgorochowik@antmicro.com>
2017-02-24 13:33:20 +0100

    Revert "procps-ng: change download site to enclustra"
    This reverts commit 5a0dc78386d44fddbaa7d2d50c75cf60f653a9c0.

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -6,7 +6,7 @@
 
 PROCPS_NG_VERSION = 3.3.10
 PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
-PROCPS_NG_SITE = ${BR2_ENCLUSTRA_SITE}
+PROCPS_NG_SITE = http://downloads.sourceforge.net/project/procps-ng/Production
 PROCPS_NG_LICENSE = GPLv2+, libproc and libps LGPLv2+
 PROCPS_NG_LICENSE_FILES = COPYING COPYING.LIB
 PROCPS_NG_INSTALL_STAGING = YES
