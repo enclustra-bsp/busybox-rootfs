32863f063fb2e6921d4006cb50edff34739f2843
Karol Gugala <kgugala@antmicro.com>
2017-03-02 10:37:11 +0100

    packages: tcf-agent: fix MACHINE flag

--- package/tcf-agent/tcf-agent.mk
+++ package/tcf-agent/tcf-agent.mk
@@ -8,12 +8,18 @@ TCF_AGENT_VERSION = 7844ce1
 TCF_AGENT_SITE = ${BR2_BACKUP_SITE}
 TCF_AGENT_SOURCE = tcf-agent-${TCF_AGENT_VERSION}.tar.gz
 
+ifeq ($(KERNEL_ARCH),arm64)
+TCF_AGENT_ARCH = a64
+else
+TCF_AGENT_ARCH = $(KERNEL_ARCH)
+endif
+
 define TCF_AGENT_BUILD_CMDS
-	AR=${TARGET_AR} CC=${TARGET_CC} OPSYS=GNU\\/Linux MACHINE=arm CFLAGS='-DENABLE_SSL=0 -DUSE_uuid_generate=0 -DENABLE_arch_armv7l' LIBS='-lpthread -lrt' ${MAKE} -C ${@D}
+	AR=${TARGET_AR} CC=${TARGET_CC} OPSYS=GNU\\/Linux MACHINE=$(TCF_AGENT_ARCH) CFLAGS='-DENABLE_SSL=0 -DUSE_uuid_generate=0 -DENABLE_arch_armv7l' LIBS='-lpthread -lrt' ${MAKE} -C ${@D}
 endef
 
 define TCF_AGENT_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/obj/GNU/Linux/arm/Debug/agent $(TARGET_DIR)/usr/bin/tcf-agent
+	$(INSTALL) -D -m 0755 $(@D)/obj/GNU/Linux/$(TCF_AGENT_ARCH)/Debug/agent $(TARGET_DIR)/usr/bin/tcf-agent
 endef
 
 $(eval $(generic-package))
