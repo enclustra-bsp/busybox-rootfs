7b346f51e4fee652a772945358af59165688a570
Andreas Buerkler <andreas.buerkler@enclustra.com>
2023-01-04 14:01:54 +0100

    Revert "Change source of deprecated version of zlib"
    This reverts commit bddf40ed97da68504fcaaf3fe70e89d1da8187d3.

--- package/libzlib/libzlib.mk
+++ package/libzlib/libzlib.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 LIBZLIB_VERSION = 1.2.11
-LIBZLIB_SOURCE = zlib-$(LIBZLIB_VERSION).tar.gz
-LIBZLIB_SITE = http://www.zlib.net/fossils
+LIBZLIB_SOURCE = zlib-$(LIBZLIB_VERSION).tar.xz
+LIBZLIB_SITE = http://www.zlib.net
 LIBZLIB_LICENSE = Zlib
 LIBZLIB_LICENSE_FILES = README
 LIBZLIB_INSTALL_STAGING = YES
